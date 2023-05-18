c27e540d793ab7bc8cd38b67e933ff17d3be54ed
CFD5906 <li.xcin.chen@gmail.com>
2023-05-19 00:27:04 +0800

    all dirs

---
+++ constant/test.f
@@ -0,0 +1,8 @@
+        type            cyclicAMI;
+        inGroups        1(cyclicAMI);
+        nFaces          11435;
+        startFace       884634;
+        matchTolerance  0.00015;
+        neighbourPatch  ami1;
+        AMIMethod       faceAreaWeightAMI;
+        restartUncoveredSourceFace 1;
